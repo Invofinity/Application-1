7269c1b6261bdae030d019437037a4ccbd5f5746
Invofinity <71259753+Invofinity@users.noreply.github.com>
2020-10-28 23:59:01 +0530

    none

---
+++ lib/pages/article_screens/discover_homescreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DiscoverHome extends StatefulWidget {
+  @override
+  _DiscoverHomeState createState() => _DiscoverHomeState();
+}
+
+class _DiscoverHomeState extends State<DiscoverHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
+}
