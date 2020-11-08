094f66aa35d839275a337098aa2ab0d9d5bc7cf7
Invofinity <veokaranews@gmail.com>
2020-11-08 15:35:03 +0530

    carousal slider changes made waiting for conformation

--- lib/pages/widgets/article-tiless.dart
+++ lib/pages/widgets/article-tiless.dart
@@ -41,99 +41,102 @@ class _ArticleTileState extends State<ArticleTile> {
                           widget.domain != null ? widget.domain.toString() : "",
                     )));
       },
-      child: Container(
-        height: 357,
-        width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.symmetric(horizontal: 2, vertical: 66),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          //color: Color(0xFFf6f6f6),
-          color: Colors.grey[850].withOpacity(0.4),
-        ),
-        alignment: Alignment.bottomLeft,
-        child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width - 46,
-                  //height: size.height * 0.20,
-                  height: MediaQuery.of(context).size.height * 0.20,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: bgColor,
-                    image: DecorationImage(
-                        image: NetworkImage(
-                          widget
-                              .thumbnail /*!= null
-                              ? widget.thumbnail.toString()
-                              : ""*/
-                          ,
-                        ),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-                SizedBox(height: 12),
-                Container(
-                  height: 124,
-                  width: MediaQuery.of(context).size.width - 46,
-                  child: AutoSizeText(
-                    widget.title /*!= null ? widget.title.toString() : ""*/,
-                    maxLines: 6,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontFamily: 'PoppinsSemiBold',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                      color: txtColor,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Container(
-                      width: 200,
-                      child: AutoSizeText("Author : " + (widget.source),
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              fontFamily: 'PoppinsSemiBold',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                              color: border1)),
-                    ),
-                    SizedBox(width: MediaQuery.of(context).size.width - 280),
-                    GestureDetector(
-                      onTap: () => _onShare(
-                          context,
-                          widget
-                              .domain /*!= null
-                              ? widget.domain.toString()
-                              : ""*/
+      child: Flexible(
+        fit: FlexFit.loose,
+        child: Container(
+          height: 357,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 2, vertical: 66),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            //color: Color(0xFFf6f6f6),
+            color: Colors.grey[850].withOpacity(0.4),
+          ),
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width - 46,
+                    //height: size.height * 0.20,
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: bgColor,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                            widget
+                                .thumbnail /*!= null
+                                ? widget.thumbnail.toString()
+                                : ""*/
+                            ,
                           ),
-                      child: Container(
-                        child: Icon(
-                          Feather.share_2,
-                          size: 16,
-                          color: border1,
-                        ),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Container(
+                    height: 124,
+                    width: MediaQuery.of(context).size.width - 46,
+                    child: AutoSizeText(
+                      widget.title /*!= null ? widget.title.toString() : ""*/,
+                      maxLines: 6,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontFamily: 'PoppinsSemiBold',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: txtColor,
                       ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-              ],
-            )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Container(
+                        width: 200,
+                        child: AutoSizeText("Author : " + (widget.source),
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontFamily: 'PoppinsSemiBold',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                                color: border1)),
+                      ),
+                      SizedBox(width: MediaQuery.of(context).size.width - 280),
+                      GestureDetector(
+                        onTap: () => _onShare(
+                            context,
+                            widget
+                                .domain /*!= null
+                                ? widget.domain.toString()
+                                : ""*/
+                            ),
+                        child: Container(
+                          child: Icon(
+                            Feather.share_2,
+                            size: 16,
+                            color: border1,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                ],
+              )),
+        ),
       ),
     );
   }
